ea7e50ccf6fbf3958d0d5f16f6af73f7b24a77a1
Alvaro Salvino <alvaro.salvino@gmail.com>
2023-10-28 10:20:35 -0300

    Update sqlAlchemyCoreApplication.py

--- sqlAlchemyCoreApplication.py
+++ sqlAlchemyCoreApplication.py
@@ -1,12 +1,12 @@
+import sqlalchemy
 from sqlalchemy import create_engine, MetaData, Table, text
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy import ForeignKey
-from sqlalchemy import inspect
 
 
-engine = create_engine('sqlite://')
+engine = create_engine('sqlite:///:memory')
 
 metadata_obj = MetaData()
 user = Table(
@@ -47,23 +47,19 @@ financial_info = Table(
     Column('value', String(100), nullable=False)
 )
 
+# Inserino info na tabela user
+sql_insert = text("Insert into user values(1, 'Maria', 'asfasfino@gmail.com', 'Ma')")
+engine.execute(sql_insert)
+
 print('\nInfo da tabela financial_info')
 print(financial_info.primary_key)
 print(financial_info.constraints)
 
 print('\nExecutando Statement sql')
 sql = text('select * from user')
-with engine.connect() as conn:
-    result = conn.execute(sql)
+result = engine.execute(sql)
 for row in result:
     print(row)
-''' metadata_obj,
-    Column('user_id', Integer, primary_key=True),
-    Column('user_name', String(40), nullable=False),
-    Column('email_address', String(60)),
-    Column('nickmane', String(50), nullable=False)
-'''
 
-sql_insert = text("Insert into user values(1, 'patrick', 'alvaro.salvino@gmail.com', 'AS')")
-with engine.connect() as conn:
-    result = conn.execute(sql_insert)
+
+
